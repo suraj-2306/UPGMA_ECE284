a2c0bac16efb271e8d0ec1756a0a121fb890413f
raj <suraj.s2306@gmail.com>
2024-03-07 22:32:51 -0800

    formatted code

--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -1,82 +1,79 @@
 #include "upgmaSerial.h"
+#include <stdio.h>
 
 using namespace std;
 
-int distMat[MATDIM][MATDIM] = {2,3,4,
-                                9,2,5,
-                                7,8,5};
+int distMat[MATDIM][MATDIM] = {2, 3, 4, 9, 2, 5, 7, 8, 5};
 
 int opMat[MATDIM][MATDIM];
 
 int clusterLst[MATDIM][MATDIM];
 
-int * matMinLoc (int d[MATDIM][MATDIM], int size) {
-    int min = __INT32_MAX__;
-    static int min_loc[2];
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j< size; j++) {
-            if (d[i][j] < min) {
-                min = d[i][j];
-                min_loc[0] = i;
-                min_loc[1] = j;
-            }
-        }
+int *matMinLoc(int d[MATDIM][MATDIM], int size) {
+  int min = __INT32_MAX__;
+  static int min_loc[2];
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (d[i][j] < min) {
+        min = d[i][j];
+        min_loc[0] = i;
+        min_loc[1] = j;
+      }
     }
+  }
 
-    return min_loc;
+  return min_loc;
 }
 
-int arrMax (int d[MATDIM], int size) {
-    int arr_max = 0;
-    for (int i = 0; i < size; i++)
-        if (d[i] > arr_max)
-            arr_max = d[i];
-    return arr_max;
+int arrMax(int d[MATDIM], int size) {
+  int arr_max = 0;
+  for (int i = 0; i < size; i++)
+    if (d[i] > arr_max)
+      arr_max = d[i];
+  return arr_max;
 }
 
-int arrMin (int d[MATDIM], int size) {
-    int arr_min = __INT_MAX__;
-    for (int i = 0; i < size; i++)
-        if (d[i] < arr_min)
-            arr_min = d[i];
-    return arr_min;
+int arrMin(int d[MATDIM], int size) {
+  int arr_min = __INT_MAX__;
+  for (int i = 0; i < size; i++)
+    if (d[i] < arr_min)
+      arr_min = d[i];
+  return arr_min;
 }
 
-int* findCluster (int clusterLst[MATDIM][MATDIM], int size, int ele1, int ele2) {
-    static int cluster_locs[2];
-    int find_count = 0;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (ele1 == clusterLst[i][j]) {
-                cluster_locs[0] = i;
-                find_count++;        
-            }
-            else if (ele2 == clusterLst[i][j]) {
-                cluster_locs[1] = i;
-                find_count++;
-            }
+int *findCluster(int clusterLst[MATDIM][MATDIM], int size, int ele1, int ele2) {
+  static int cluster_locs[2];
+  int find_count = 0;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (ele1 == clusterLst[i][j]) {
+        cluster_locs[0] = i;
+        find_count++;
+      } else if (ele2 == clusterLst[i][j]) {
+        cluster_locs[1] = i;
+        find_count++;
+      }
 
-            if (find_count == 2)
-                return cluster_locs;
-        }
+      if (find_count == 2)
+        return cluster_locs;
     }
-    
-    return cluster_locs;
+  }
+
+  return cluster_locs;
 }
 
-void grpLeaf (int clusterLst[MATDIM][MATDIM], int size, int ele1, int ele2) {
-    for (int i = 0; i < size; i++) {
-        int 
-    }
+void grpLeaf(int clusterLst[MATDIM][MATDIM], int size, int ele1, int ele2) {
+  for (int i = 0; i < size; i++) {
+  }
 }
 
 int main() {
 
-    int *loc;
+  int *loc;
 
-    loc = matMinLoc(distMat, MATDIM);
+  loc = matMinLoc(distMat, MATDIM);
 
-    printf("%d, %d\n", loc[0], loc[1]);
+  printf("%d, %d\n", loc[0], loc[1]);
 
-    return 0;
+  return 0;
 }
--- src/serial/upgmaSerial.h
+++ src/serial/upgmaSerial.h
@@ -7,7 +7,7 @@
 // extern int clusterLst[MATDIM][MATDIM];
 
 // Accepts a 2D matrix and returns a matrix mirror along the left diagonal
-void mirrorMat (int size);
+void mirrorMat(int size);
 
 // Returns a list of lists of length equal to the dimension of (square) distance
 // matrix. Can implement as a fixed 2D matrix, or linked list. Update the
@@ -16,24 +16,25 @@ void initClusterLst(int size);
 
 // Return the indexes corresponding to the smallest value in the matrix.
 // Return smallest value if required.
-int* matMinLoc (int size);
+int *matMinLoc(int size);
 
 // Returns the maximum from an array. Use standard functions instead?
-int arrMax (int size);
+int arrMax(int size);
 
-// Combine the 2 clusters in clusterList that contain ele1 and ele2, IF they belong to separate clusters
-void grpLeaf ( int size, int ele1, int ele2);
+// Combine the 2 clusters in clusterList that contain ele1 and ele2, IF they
+// belong to separate clusters
+void grpLeaf(int size, int ele1, int ele2);
 
-// Return an array of 2 elements that represent the location of ele1 and ele2 in the cluster list of lists.
-// e.g. [ [0, 3], [1, 5], [2], [4], [6] ]
-// if ele1 = 3 and ele2 = 5, the result should be [0,1]
-int* findCluster (int size, int ele1, int ele2);
+// Return an array of 2 elements that represent the location of ele1 and ele2 in
+// the cluster list of lists. e.g. [ [0, 3], [1, 5], [2], [4], [6] ] if ele1 = 3
+// and ele2 = 5, the result should be [0,1]
+int *findCluster(int size, int ele1, int ele2);
 
 // Recompute distances after cluster update
 void matEntryUpdate(int size, int ele1, int ele2);
 
 // Print the 2D distance matrix
-void printMat (int size);
+void printMat(int size);
 
 // Print the cluster list
-void printClusterLst (int size);
+void printClusterLst(int size);
